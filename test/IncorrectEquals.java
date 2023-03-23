1db80ab0bb5eab68db392fa4d847b5e31367ce54
kv-zayceva <115557216+kv-zayceva@users.noreply.github.com>
2023-03-23 17:19:13 +0300

    IncorrectEquals.java
    add eng

--- test/IncorrectEquals.java
+++ test/IncorrectEquals.java
@@ -6,8 +6,8 @@ public class IncorrectEquals {
 
     @Test
     public void should_return_equals() {
-        Item item1 = new Item("яблоки", 50);
-        Item item2 = new Item("яблоки", 50);
+        Item item1 = new Item("apples", 50);
+        Item item2 = new Item("apples", 50);
 
         assertEquals(item2, item1);
     }
